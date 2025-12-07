af098b9a8098eddc615074e6252548d7e265ff86
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-12-07 20:14:11 +0100

    Start of the repoitory pattern

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"log"
 	"tiagoapi/internal/env"
+	"tiagoapi/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
