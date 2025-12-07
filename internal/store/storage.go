affdfececd484e4dd044394eb884265f9571a3e8
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-12-07 20:39:50 +0100

    Add postgresql driver + Models + Repositories Create implementation

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,20 +3,21 @@ package store
 import (
 	"context"
 	"database/sql"
+	"tiagoapi/internal/model"
 )
 
 type Storage struct {
 	Posts interface {
-		Create(context.Context) error
+		Create(context.Context, *model.Post) error
 	}
 	Users interface {
-		Create(context.Context) error
+		Create(context.Context, *model.User) error
 	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostsStore{db},
-		Users: &UsersStore{db},
+		Posts: &PostStore{db},
+		Users: &UserStore{db},
 	}
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,12 +3,34 @@ package store
 import (
 	"context"
 	"database/sql"
+	"tiagoapi/internal/model"
 )
 
-type UsersStore struct {
+type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UserStore) Create(ctx context.Context, user *model.User) error {
+	query := `
+		INSERT INTO users (username, password, email)
+		VALUES ($1, $2, $3)
+		RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
