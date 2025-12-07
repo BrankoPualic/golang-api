55db0700904da24ee062cc53643b7b014f5489d5
Branko Pualić-Radujko <pualicbranko@gmail.com>
2025-12-07 19:53:09 +0100

    Add environment variable. Accessed through direnv cli

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "log"
+import (
+	"log"
+	"tiagoapi/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
